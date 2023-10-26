c6d2b94419c3747d85cdf9cab945dd967c242182
GeonwooVincentKim <kdsnop@gmail.com>
2023-10-26 16:01:07 +0900

    [Update] - 0

--- WriteCode/CodePractice/test.c
+++ WriteCode/CodePractice/test.c
@@ -4,9 +4,12 @@ int main() {
 //    int A[] = {2, 4, 6, 8, 10, 12, 14};  // Place 4 bytes by each location
     int A[10] = {0};
     
-    printf("%lu\n", sizeof(A));
-    printf("%d\n", A[2]);
-    printf("%d\n", A[3]);
+    for (int i = 0; i < 10; i++) 
+        printf("%d\n", A[i]);
+    
+//    printf("%lu\n", sizeof(A));
+//    printf("%d\n", A[2]);
+//    printf("%d\n", A[3]);
     
     return 0;
 }
