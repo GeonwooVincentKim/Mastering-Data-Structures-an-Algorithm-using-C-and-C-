2bc0f1de21939527b5ba418eef8d1ba268bb835a
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-04 19:35:21 +0900

    test

--- WriteCode/BaekTest/main.c
+++ WriteCode/BaekTest/main.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-        
-    if (a < b) 
-        printf("<");
-    else if (a > b)
-        printf(">");
-    else
-        printf("==");
-    
+int main() {   
     return 0;
 }
