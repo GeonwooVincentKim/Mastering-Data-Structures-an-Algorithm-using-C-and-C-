9e873f987b26bb6631566308230a5a91b8b5cebf
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-06 10:39:35 +0900

    [Feature] - If pharse

--- WriteCode/BaekTest/main.c
+++ WriteCode/BaekTest/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main() {   
+int main() {
+    int a, b;
+    scanf("%d\n", &a);
+    scanf("%d", &b);
+    
+    if (a > 0 && b > 0)
+        printf("1");
+    else if (a > 0 && b < 0)
+        printf("4");
+    else if (a < 0 && b < 0)
+        printf("3");
+    else if (a < 0 && b > 0)
+        printf("2");
+    
     return 0;
 }
