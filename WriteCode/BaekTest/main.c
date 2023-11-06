d6adea7df931467729a26bc4d4418fe3691bdadc
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-06 10:52:10 +0900

    [Feature] - Yoon

--- WriteCode/BaekTest/main.c
+++ WriteCode/BaekTest/main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d\n", &a);
-    scanf("%d", &b);
+    int a;
+    scanf("%d", &a);
     
-    if (a > 0 && b > 0)
+    if ((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
         printf("1");
-    else if (a > 0 && b < 0)
-        printf("4");
-    else if (a < 0 && b < 0)
-        printf("3");
-    else if (a < 0 && b > 0)
-        printf("2");
+    else
+        printf("0");
     
     return 0;
 }
