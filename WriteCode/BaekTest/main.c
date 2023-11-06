30a8fdb1698c5101b6b9fd0e28f0a6d49bf076a7
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-06 16:06:30 +0900

    [update] - gugu

--- WriteCode/BaekTest/main.c
+++ WriteCode/BaekTest/main.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int t;
-    scanf("%d", &t);
+    int n;
+    scanf("%d", &n);
     
-    int i, a, b, sum = 0;
-    for (i = 0; i < t; i++) {
-        scanf("%d %d", &a, &b);
-        sum = a + b;
-        printf("%d\n", sum);
-        sum = 0;
+    int i;
+    for (i = 1; i <= 9; i++) {
+       printf("%d * %d = %d\n", n, i, n * i);
     }
     
     return 0;
