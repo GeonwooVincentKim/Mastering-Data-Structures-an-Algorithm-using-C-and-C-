fbafe55f493c59e9ab6ec9a03a701a67446c4a23
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-07 17:15:34 +0900

    [Update] - Correct way

--- WriteCode/BaekTestElementary/main.c
+++ WriteCode/BaekTestElementary/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int stConvertArr[10];
+
 int main() {
     int a, b, c;
     scanf("%d", &a);
@@ -8,40 +10,13 @@ int main() {
     
     int temp;
     temp = a * b * c;
+
+    int i, j;
+    for (i = temp; i > 0; i /= 10) 
+        stConvertArr[i % 10]++;
     
-    char stConvertArr[20];
-    sprintf(stConvertArr, "%d", temp);
-    
-    printf("%s\n", stConvertArr);
-    
-    int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0, count9 = 0;
-    
-    int i;
-    for (i = 0; stConvertArr[i] != '\0'; i++) {
-        printf("%d", stConvertArr[i]);
-        
-        if (stConvertArr[i] == 1) {
-            count1++;
-        } else if (stConvertArr[i] == 2) {
-            count2++;
-        } else if (stConvertArr[i] == 3) {
-            count3++;
-        } else if (stConvertArr[i] == 4) {
-            count4++;
-        } else if (stConvertArr[i] == 5) {
-            count5++;
-        } else if (stConvertArr[i] == 6) {
-            count6++;
-        } else if (stConvertArr[i] == 7) {
-            count7++;
-        } else if (stConvertArr[i] == 8) {
-            count8++;
-        } else if (stConvertArr[i] == 9) {
-            count9++;
-        }
-    }
-    
-    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", count1, count2, count3, count4, count5, count6, count7, count8, count9);
+    for (j = 0; j < 10; j++) 
+        printf("%d\n", stConvertArr[j]);
     
     return 0;
 }
