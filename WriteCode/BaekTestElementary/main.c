9543f3d91be940a8a451ec0aba11903390e5d577
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-07 16:42:36 +0900

    [Update] - Power

--- WriteCode/BaekTestElementary/main.c
+++ WriteCode/BaekTestElementary/main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    int n_arr[5];
     
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (j < n - i - 1) printf(" ");
-            else printf("*");
-        }
-        printf("\n");
+    int i, power = 1, j = 0, sum = 0;
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &n_arr[i]);
+        
+        for (j = 0; j < 2; j++)
+            power = power * n_arr[i];
+
+        sum += power;
+        power = 1;
     }
     
+    printf("%d", sum % 10);
     return 0;
 }
+
+//#include <stdio.h>
+//int main(){
+//    int a, b, i, inv=1; 
+//    //inv는 곱셈에 사용하므로 '1'로 초기화 
+//    printf("==거듭제곱 출력 프로그램==\n\n"); 
+//    printf("밑수를 입력하세요."), scanf("%d", &a);
+//    printf("지수를 입력하세요."), scanf("%d", &b);
+//    
+//    for(i=0;i<b;i++) {
+//        //입력받은 지수만큼 반복
+//        inv=inv*a;      //a의 거듭제곱
+//    }
+//    printf("\n%d의 %d제곱은 %d입니다.", a, b, inv);
+//return 0;}
+//
