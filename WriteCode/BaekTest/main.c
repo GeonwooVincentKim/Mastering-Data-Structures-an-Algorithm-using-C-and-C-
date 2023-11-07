d6883da86865c9aaaa7eeccf9f5692663a3029e1
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-07 14:03:36 +0900

    [Feature] - Max and Min

--- WriteCode/BaekTest/main.c
+++ WriteCode/BaekTest/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int t;
-    scanf("%d", &t);
+    int n;
+    scanf("%d", &n);
     
-    int i, index, a = 0;
-    int arr[9];
+    int i;
+    int st[n];
+    int min, max;
     
-    for (i = 0; i < 9; i++) {
-        scanf("%d", &arr[i]);
-        
-        if (a < arr[i]) {
-            a = arr[i];
-            index = i;
-        }
+    for (i = 0; i < n; i++) {
+        scanf("%d", &st[i]);
+    
+        if (i == 0) max = min = st[i];
+        if (st[i] > max) max = st[i];
+        if (st[i] < min) min = st[i];
     }
-        
-    printf("%d\n%d", a, index + 1);
-
+    
+    printf("%d %d", min, max);
+    
     return 0;
 }
