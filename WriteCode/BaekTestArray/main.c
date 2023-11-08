79a00f8c12897352a06de7224956ca23adfe804b
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-08 14:29:35 +0900

    [Update] - Updated source code

--- WriteCode/BaekTestArray/main.c
+++ WriteCode/BaekTestArray/main.c
@@ -26,38 +26,16 @@ int isDescending(int *arr, int size) {
 
 int main() {
     int arr[8];
-    
-    int i, val;
-    int a, b;
-//    char[] result = "";
-    
-    for (i = 0; i < 8; i++){
-        scanf("%d", &arr[i]);
-        val = arr[0];
-    }
+    int i, a, b;
+
+    for (i = 0; i < 8; i++) scanf("%d", &arr[i]);
     
     a = isAscending(arr, 8);
     b = isDescending(arr, 8);
     
-    printf("%d %d", a, b);
-    
     if (a == 1) printf("ascending");
     if (b == 1) printf("descending");
     if (a == 0 && b == 0) printf("mixed");
-    
-    
-//    printf("%s", 
-    
-//    for (i = 0; i < 8; i++) {
-//        printf("%d ", arr[i]);
-//    }
-//    for (i = 0; i < 8; i++) {
-//        if (val > arr[i]) printf("ascending");
-//        else if (val < arr[i]) printf("descending");
-//        else printf("mixed");
-//    }
-    
-//    printf("%d", arr[0]);
-    
+
     return 0;
 }
