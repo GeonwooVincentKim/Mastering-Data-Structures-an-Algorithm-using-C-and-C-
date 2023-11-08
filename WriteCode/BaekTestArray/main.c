ee32744242704b03db5ec82f9f8b055193e65e92
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-08 17:02:28 +0900

    [Update] - Main

--- WriteCode/BaekTestArray/main.c
+++ WriteCode/BaekTestArray/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
+    int t;
+    scanf("%d", &t);
     
-    int line1st = b % 10 * a;
-    int line2nd = (b % 100) / 10 * a;
-    int line3rd = (b / 100) * a;
+    int i, j, count = 0, sum = 0;
+    char str[80];
     
-    printf("%d\n%d\n%d\n%d", line1st, line2nd, line3rd, a * b);
+    for (i = 0; i < t; i++){
+        scanf("%s", str);
+        
+        j = 0;
+        count = 1;
+        sum = 0;
+        
+        for (j = 0; str[j] != '\0'; j++) {
+            if (str[j] == 'O') {
+                sum += count;
+                count += 1;
+            }
+            
+            if (str[j] == 'X') count = 1;
+        }
+        
+        printf("%d\n", sum);
+    }
 }
