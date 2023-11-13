8ba5101f6a6cdf1d19bec005ee1cd071a87240fb
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-13 10:09:35 +0900

    [Update] - Rock game

--- WriteCode/NormalPractice/main.c
+++ WriteCode/NormalPractice/main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-    int h, m, c;
-    scanf("%d %d", &h, &m);
-    scanf("%d", &c);
-
-    if (m + c < 60) {
-        printf("%d %d", h, m + c);
-    } else {
-        int hour = (m + c) / 60;
-        int min = (m + c) % 60;
-        
-        if (h + hour < 24) printf("%d %d", h + hour, min);
-        else printf("%d %d", h + hour - 24, min);
-    }
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    if (n % 7 == 1 || n % 7 == 3) printf("CY");
+    else printf("SK");
     
     return 0;
 }
