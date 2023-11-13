700b04fbd6cce4e8df057793cfab3e0b14fcd71c
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-13 10:25:22 +0900

    [Update] - Normal

--- WriteCode/NormalPractice/main.c
+++ WriteCode/NormalPractice/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    long long int n;
+    scanf("%lld", &n);
     
-    if (n % 7 == 1 || n % 7 == 3) printf("CY");
+    if (n % 7 == 0 || n % 7 == 2) printf("CY");
     else printf("SK");
     
     return 0;
