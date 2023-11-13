903df87e57e7801bea3c5c0723e9f9e8e4638ac7
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-13 13:29:19 +0900

    [Update] - Gap

--- WriteCode/NormalPractice/main.c
+++ WriteCode/NormalPractice/main.c
@@ -1,26 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
+    int n;
+    scanf("%d", &n);
     
-    char aStr[4], bStr[4];
-    
-    sprintf(aStr, "%d", a);
-    sprintf(bStr, "%d", b);
-    
-    char tempA = aStr[0];
-    aStr[0] = aStr[2];
-    aStr[2] = tempA;
-    
-    char tempB = bStr[0];
-    bStr[0] = bStr[2];
-    bStr[2] = tempB;
-    
-    if (strcmp(aStr, bStr) > 0) printf("%s", aStr);
-    else printf("%s", bStr);
-    
+    int i, gap;    
+    for (i = 1; i <= n; i++) {
+        gap = i + 1;
+        if (30 % gap == 0) printf("%d\n", i);
+    }
+
     return 0;
 }
