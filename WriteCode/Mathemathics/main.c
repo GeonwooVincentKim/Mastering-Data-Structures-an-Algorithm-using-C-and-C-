1b42d6d57d3b1c219ca7d7aff559a04fcdf3609f
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-17 16:31:32 +0900

    [Update] - Game Theory

--- WriteCode/Mathemathics/main.c
+++ WriteCode/Mathemathics/main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int n;
+    scanf("%d", &n);
     
-    while (1) {
-        scanf("%d %d", &a, &b);
-        int a1 = a, b1 = b;
-        if (a1 == 0 && b1 == 0) break;
-        
-        if (a1 <= b1) {
-            if (b1 % a1 == 0) printf("factor\n");
-        } else {
-            if (a1 % b1 == 0) printf("multiple\n");
-            else printf("neither\n");
-        }
-    }
-
+    if (n % 2 == 0) printf("Duck");
+    else if (n % 2 == 1) printf("Goose");
+    
     return 0;
 }
