1f5ba4e1621d96951fa498f14964172f3e6ab6ef
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-21 14:30:18 +0900

    [Update] - Rectangle

--- WriteCode/Mathemathics/main.c
+++ WriteCode/Mathemathics/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int k;
-    scanf("%d", &k);
+    int x, y, w, h;
+    scanf("%d %d %d %d", &x, &y, &w, &h);
     
-    if (k == 2 || k == 6) printf("Kali");
-    else printf("Ringo");
+    if ((w - x) > (h - y)) {
+        if ((h - y) > x) printf("%d", (x > y) ? y : x);
+        else if (h - y > y) printf("%d", (y > x) ? x : y);
+        else printf("%d", h - y);
+    } else {
+        if((w - x) > x) printf("%d", (x > y) ? y : x);
+        else if((w - x) > y) printf("%d", (y > x) ? x : y);
+        else printf("%d", w - x);
+    }
     
     return 0;
 }
