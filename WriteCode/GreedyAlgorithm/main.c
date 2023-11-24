505248c53e85bdfea24035a1b92296dd4fc1d165
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-24 16:04:03 +0900

    [Update] - change

--- WriteCode/GreedyAlgorithm/main.c
+++ WriteCode/GreedyAlgorithm/main.c
@@ -4,18 +4,22 @@ int main() {
     int n, i, j, count = 0;
     scanf("%d", &n);
     
-    int greedyList[4] = {25, 10, 5, 1};
+    int greedyList[6] = {500, 100, 50, 10, 5, 1};
     
     int m;
     for (j = 0; j < n; j++) {
         scanf("%d", &m);
+        m = 1000 - m; // change
+        
         for (i = 0; i < sizeof(greedyList) / sizeof(int); i++) {
     //        printf("%d ", greedyList[i]);
-            printf("%d ", m / greedyList[i]);
+//            printf("%d ", m / greedyList[i]);
             count += m / greedyList[i];
+            
+//            printf("%d ", m % greedyList[i]);
             m %= greedyList[i];
         }
-//        printf("%d\n", count);
+        printf("%d\n", count);
     }
 
     
