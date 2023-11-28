94d429d44a23ce56b8e21c8a7cbec9d9ea32c484
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-28 11:05:20 +0900

    test:

--- WriteCode/BaekTestElementary/main.c
+++ WriteCode/BaekTestElementary/main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
-int stConvertArr[10];
-
 int main() {
-    int a, b, c;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    scanf("%d", &c);
+    char subjectName[51];
+    char score[3];
+    double credit;
     
-    int temp;
-    temp = a * b * c;
-
-    int i, j;
-    for (i = temp; i > 0; i /= 10) 
-        stConvertArr[i % 10]++;
+    int i;
+    int creditSum = 0;
+    double gradeNum = 0, sum = 0;
     
-    for (j = 0; j < 10; j++) 
-        printf("%d\n", stConvertArr[j]);
+    for (i < 0; i < 20; i++) {
+        scanf("%s %lf %s", subjectName, &credit, score);
+        
+        if (score[0] == 'P') continue;
+        creditSum += credit;
+        
+        if (score[0] == 'F') continue;
+        
+        if (score[0] == 'A') gradeNum = 4;
+        else if (score[0] == 'B') gradeNum = 3;
+        else if (score[0] == 'C') gradeNum = 2;
+        else gradeNum = 1;
+        
+        if (score[1] == '+') gradeNum += 0.5;
+        sum += credit * gradeNum;
+    }
+    
+    printf("%lf\n", sum / creditSum);
     
     return 0;
 }
