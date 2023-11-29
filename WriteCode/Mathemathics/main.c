4aa26588fb26475191166c646e7817fdbfd8916d
GeonwooVincentKim <kdsnop@gmail.com>
2023-11-29 14:15:41 +0900

    [Update] - Updated codes

--- WriteCode/Mathemathics/main.c
+++ WriteCode/Mathemathics/main.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int i, n;
-    scanf("%d", &n);
+    int n, m, lcm, i, gcd;
+    scanf("%d %d", &n, &m);
     
-    for (i = 2; i <= n; i++) {
-        if (n % i == 0) {
-            printf("%d", i);
-            n /= i;
-            
-            if (n % i == 0) printf("\n");
-            else if (n % i != 0) {
-                if (n > i) printf("\n");
-            }
-            
-            i = 1;
-        }
+    for (i = 1; i <= n && i <= m; i++) {
+        if (n % i == 0 && m % i == 0) gcd = i;
     }
+    
+    i = 0;
+    for (i = n * m; i >= 1; i--) {
+        if (i % n == 0 && i % m == 0) lcm = i;
+    }
+    
+    printf("%d\n%d", gcd, lcm);
     
     return 0;
 }
